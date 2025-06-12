ad7f7937500ff4d86c3da9cff76a2a2d895aa7e7
Rebecca Molund Labeeb <rebecca.labeeb@gmail.com>
2025-06-12 21:51:25 +0200

    Added som posts from a fake API to the homescreen

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,7 +1,30 @@
+import PostsList from "@/components/Posts";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 import { StyleSheet, View } from "react-native";
 
 export default function TabTwoScreen() {
-  return <View>Hem</View>;
+  return (
+    <ThemedView style={styles.container}>
+      <View>
+        <ThemedText type="defaultSemiBold">Hem</ThemedText>
+        <PostsList />
+      </View>
+    </ThemedView>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: "center",
+  },
+
+  content: {
+    flex: 1,
+    padding: 32,
+    gap: 16,
+    overflow: "hidden",
+  },
+});
