6fc85107ab6c65fa549a51d59eec9187ffb627a9
DianaOlivaresBarahona <olivaresbarahona@live.se>
2025-06-13 13:31:38 +0200

    fixed onboarding and updated login

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import PostsList from "@/components/Posts";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { StyleSheet } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Välkommen hem!</Text>
-    </View>
+    <ThemedView style={styles.container}>
+      <ThemedText type="defaultSemiBold">Hem</ThemedText>
+      <PostsList />
+    </ThemedView>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    alignItems: "center",
+    overflow: "hidden",
+  },
+});
---
+++ components/Posts.tsx
@@ -0,0 +1,89 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+const PostsList = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  type Post = {
+    id: number;
+    title: string;
+    body: string;
+    tags: string[];
+    reactions: {
+      likes: number;
+      dislikes: number;
+    };
+    views: number;
+    userId: number;
+  };
+  useEffect(() => {
+    axios
+      .get("https://dummyjson.com/posts")
+      .then((response) => {
+        setPosts(response.data.posts);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Fel vid API-anrop:", error);
+        setLoading(false);
+      });
+  }, []);
+  if (loading) {
+    return <ActivityIndicator size="large" color="#007AFF" />;
+  }
+  const renderItem = ({ item }: { item: Post }) => (
+    <View style={styles.postContainer}>
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={styles.body}>{item.body}</Text>
+      <Text style={styles.tags}>Tags: {item.tags.join(", ")}</Text>
+      <Text style={styles.meta}>
+        :+1: {item.reactions.likes} | :-1: {item.reactions.dislikes} | :eye:{" "}
+        {item.views}
+      </Text>
+    </View>
+  );
+  return (
+    <FlatList
+      data={posts}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={renderItem}
+      contentContainerStyle={styles.list}
+    />
+  );
+};
+const styles = StyleSheet.create({
+  list: {
+    padding: 16,
+  },
+  postContainer: {
+    marginBottom: 20,
+    backgroundColor: "#F4F4F4",
+    padding: 16,
+    borderRadius: 12,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 16,
+    marginBottom: 6,
+  },
+  body: {
+    fontSize: 14,
+    marginBottom: 6,
+  },
+  tags: {
+    fontStyle: "italic",
+    color: "#555",
+    marginBottom: 4,
+  },
+  meta: {
+    fontSize: 12,
+    color: "#999",
+  },
+});
+export default PostsList;
