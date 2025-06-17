6c2ae27eb397cff8f40a26c8441b0e9b6117043c
Rebecca Molund Labeeb <rebecca.labeeb@gmail.com>
2025-06-17 13:23:27 +0200

    Added new illustrations and text to the onboarding carousel

--- components/OnboardingScreen.tsx
+++ components/OnboardingScreen.tsx
@@ -1,7 +1,7 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useRouter } from "expo-router";
 import React from "react";
-import { Image } from "react-native";
+import { Image, StyleSheet } from "react-native";
 import Onboarding from "react-native-onboarding-swiper";
 
 const OnboardingScreen: React.FC = () => {
@@ -9,53 +9,75 @@ const OnboardingScreen: React.FC = () => {
 
   const finishOnboarding = async () => {
     await AsyncStorage.setItem("hasSeenOnboarding", "true");
-    router.replace("/home"); // Skicka vidare till hemskärmen
+    router.replace("/home");
   };
 
   return (
     <Onboarding
       onSkip={finishOnboarding}
       onDone={finishOnboarding}
+      transitionAnimationDuration={500}
+      titleStyles={styles.title}
+      subTitleStyles={styles.subtitle}
       pages={[
         {
-          backgroundColor: "#fff",
+          backgroundColor: "#f0f4ff",
           image: (
             <Image
-              source={require("../assets/images/onboarding.jpg")}
-              style={{ width: 200, height: 200 }}
+              source={require("../assets/images/welcome.jpg")}
+              style={styles.image}
               resizeMode="contain"
             />
           ),
           title: "Välkommen!",
-          subtitle: "Det här är en enkel onboarding",
+          subtitle: "Har du tråkigt? Här blir det rävligt kul!",
         },
         {
-          backgroundColor: "#fdeb93",
+          backgroundColor: "#fff3e6",
           image: (
             <Image
-              source={require("../assets/images/onboarding.jpg")}
-              style={{ width: 200, height: 200 }}
+              source={require("../assets/images/explore.jpg")}
+              style={styles.image}
               resizeMode="contain"
             />
           ),
           title: "Utforska",
-          subtitle: "Vi visar dig runt i appen",
+          subtitle: "Anakin hatar sand, men vi? Vi kamel-lar genom öknen.",
         },
         {
-          backgroundColor: "#e9bcbe",
+          backgroundColor: "#ffeef2",
           image: (
             <Image
-              source={require("../assets/images/onboarding.jpg")}
-              style={{ width: 200, height: 200 }}
+              source={require("../assets/images/get-started.jpg")}
+              style={styles.image}
               resizeMode="contain"
             />
           ),
           title: "Kom igång!",
-          subtitle: "Tryck på 'Klar' för att starta",
+          subtitle:
+            "Skogstokig? Då har du kommit helt rätt! Tryck på 'Klar' för att börja din resa.",
         },
       ]}
     />
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: 280,
+    height: 240,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  subtitle: {
+    fontSize: 16,
+    paddingHorizontal: 20,
+    textAlign: "center",
+    color: "#555",
+  },
+});
 
 export default OnboardingScreen;
